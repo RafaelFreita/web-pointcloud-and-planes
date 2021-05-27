bfff479fffab5901b4f3b0441063544b7d245eaa
RafaelFreita <dfr.rafael@gmail.com>
2021-05-27 18:37:04 -0300

    Not sure what i did, just backuping

--- frontend/src/color.js
+++ frontend/src/color.js
@@ -9,10 +9,10 @@ function rgb2hsv(r, g, b) {
 function hsv2rgb(
   h,
   s,
-  v,
-  f = (n, k = (n + h / 60) % 6) =>
-    v - v * s * Math.max(Math.min(k, 4 - k, 1), 0)
+  v
 ) {
+  const f = (n, k = (n + h / 60) % 6) =>
+    v - v * s * Math.max(Math.min(k, 4 - k, 1), 0)
   return [f(5), f(3), f(1)];
 }
 
