97b652c9ba440059ad901b73173400ebe0facfaf
RafaelFreita <dfr.rafael@gmail.com>
2021-07-12 14:03:28 -0300

    Finishing

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,9 +15,18 @@ import {
   MeshBasicMaterial,
   DoubleSide,
   Mesh,
+  BackSide,
+  FrontSide,
+  MeshLambertMaterial,
+  AmbientLight,
+  PointLight,
+  Color,
+  Group,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { XYZLoader } from "three/examples/jsm/loaders/XYZLoader.js";
+import { ConvexGeometry } from "three/examples/jsm/geometries/ConvexGeometry.js";
+import Delaunator from "delaunator";
 
 import axios from "axios";
 
@@ -36,16 +45,19 @@ function loadPlanes(id) {
     scene.remove(planeMesh.mesh);
   }
   planeMeshes.length = 0;
-  api(`${id}/planes`).then((res) => {
-    // Cleaning planes
-    // Only using EVAL because the data source is reliable.
-    // TODO: Change files to JSON instead of JS and use JSON.parse instead of eval
-    const planesData = eval(res.data);
-    planes = generatePlanes(planesData[0].surface_roughness_data.node);
+  return new Promise((resolve, reject) => {
+    api(`${id}/planes`).then((res) => {
+      // Cleaning planes
+      // Only using EVAL because the data source is reliable.
+      // TODO: Change files to JSON instead of JS and use JSON.parse instead of eval
+      const planesData = eval(res.data);
+      planes = generatePlanes(planesData[0].surface_roughness_data.node);
 
-    generatePlanesMeshes();
-    //changePlanesVisibility();
-    //configurePlanesGUI();
+      generatePlanesMeshes();
+      resolve();
+      //changePlanesVisibility();
+      //configurePlanesGUI();
+    });
   });
 }
 
@@ -84,23 +96,28 @@ api.get("").then((res) => {
   }
 
   gui.add(guiConfig.general, "model", modelsObject).onChange(async () => {
-    await loadPoints(guiConfig.general.model);
-    loadPlanes(guiConfig.general.model);
+    loadModel(guiConfig.general.model);
   });
 });
 
-let points, pointsColors, pointsMaterial;
+let points,
+  pointsColors = [],
+  pointsMaterial;
 const cloudCenter = new Vector3();
 const planeMeshes = [];
 const planeControls = [];
 
 let controls;
 
+async function loadModel(id) {
+  await loadPlanes(id);
+  await loadPoints(id);
+}
+
 configureGui();
 init();
-loadPoints(MODEL_ID).then((_) => {
-  loadPlanes(MODEL_ID);
-});
+loadModel(MODEL_ID);
+
 animate();
 
 // Gui config
@@ -150,6 +167,29 @@ function configureGui() {
   planesFolder
     .add(guiConfig.planes, "visible")
     .onChange(changePlanesVisibility);
+}
+
+function pointCloudColorsByDepthComparison(points, axis = 0, comparisonValue) {
+  // Generate color array
+  const colors = [];
+
+  const array = points.geometry.attributes.position.array;
+
+  const minColor = guiConfig.points.minColor.map((v) => v / 255);
+  const midColor = guiConfig.points.midColor.map((v) => v / 255);
+  const maxColor = guiConfig.points.maxColor.map((v) => v / 255);
+
+  for (let i = axis; i < array.length; i += 3) {
+    const pointDepth = array[i];
+    const pointValue = comparisonValue - pointDepth;
+    const pointColor = pointValue < 0 ? minColor : maxColor;
+
+    colors.push(pointColor[0]); // R
+    colors.push(pointColor[1]); // G
+    colors.push(pointColor[2]); // B
+  }
+
+  return colors;
 }
 
 function pointCloudColorsByDepth(points, axis = 0) {
@@ -250,12 +290,17 @@ function init() {
 
   axesHelper = new AxesHelper(5);
   scene.add(axesHelper);
+
+  scene.add(new AmbientLight(0xffffff));
+
+  const light = new PointLight(0xffffff, 1);
+  camera.add(light);
 }
 
+let mesh;
 function loadPoints(id) {
-  if (points) {
-    scene.remove(points);
-  }
+  if (points) scene.remove(points);
+  if (mesh) scene.remove(mesh);
 
   const loader = new XYZLoader();
   return new Promise((resolve, reject) => {
@@ -263,6 +308,9 @@ function loadPoints(id) {
       geometry.computeBoundingBox();
       geometry.boundingBox.getCenter(cloudCenter);
       geometry.center();
+
+      // Set planes position
+      planesGroup.position.copy(cloudCenter.multiplyScalar(-1));
 
       pointsMaterial = new PointsMaterial({
         size: guiConfig.points.size,
@@ -273,13 +321,82 @@ function loadPoints(id) {
       setPointsMaterialGUI();
 
       points = new Points(geometry, pointsMaterial);
-      pointsColors = pointCloudColorsByDepth(points, 2);
-      geometry.setAttribute(
-        "color",
-        new Float32BufferAttribute(pointsColors, 3)
-      );
+      setPointsVertexColor();
 
       scene.add(points);
+
+      // Get vertices from point cloud
+
+      const vertices = [];
+      const positionAttribute = geometry.getAttribute("position");
+
+      for (let i = 0; i < positionAttribute.count; i++) {
+        const vertex = new Vector3();
+        vertex.fromBufferAttribute(positionAttribute, i);
+        vertices.push(vertex);
+      }
+
+      // Generate mesh
+
+      //const meshGeometry = new BufferGeometry().setFromPoints(vertices);
+
+      // triangulate x, z
+      let indexDelaunay;
+      if (guiConfig.points.axis === 0) {
+        indexDelaunay = Delaunator.from(
+          vertices.map((v) => {
+            return [v.y, v.z];
+          })
+        );
+      } else if (guiConfig.points.axis === 1) {
+        indexDelaunay = Delaunator.from(
+          vertices.map((v) => {
+            return [v.x, v.z];
+          })
+        );
+      } else {
+        indexDelaunay = Delaunator.from(
+          vertices.map((v) => {
+            return [v.x, v.y];
+          })
+        );
+      }
+
+      var meshIndex = []; // Convert index to ThreeJS index
+      for (let i = 0; i < indexDelaunay.triangles.length; i++) {
+        meshIndex.push(indexDelaunay.triangles[i]);
+      }
+
+      geometry.setIndex(meshIndex); // change indexes in geometry
+      geometry.computeVertexNormals();
+
+      // Create material
+
+      const meshMaterial = new MeshLambertMaterial({
+        vertexColors: true,
+        wireframe: true,
+      });
+
+      // const wireframeMaterial = new MeshBasicMaterial({
+      //   color: 0xffffff,
+      //   wireframe: true,
+      //   transparent: true,
+      // });
+
+      // Generate geometry
+
+      //var meshGeometry  = new ConvexGeometry(vertices)
+
+      mesh = new Mesh(geometry, meshMaterial);
+      // mesh1.material.side = BackSide; // back faces
+      // mesh1.renderOrder = 0;
+      scene.add(mesh);
+
+      // const mesh2 = new Mesh(meshGeometry, wireframeMaterial.clone());
+      // mesh2.material.side = FrontSide; // front faces
+      // mesh2.renderOrder = 1;
+      // scene.add(mesh2);
+
       resolve();
     });
   });
@@ -290,12 +407,27 @@ function setPointsMaterialGUI() {
 }
 
 function setPointsVertexColor() {
+  if (planes && planes.length > 0) {
+    const accessMapPlane = { 0: "x", 1: "z", 2: "y" };
+    const accessMapCloud = { 0: "x", 1: "y", 2: "z" };
+
+    const planeCenter = planes[0].center;
+    const planeCenterPos =
+      planeCenter[accessMapPlane[guiConfig.points.axis]] +
+      cloudCenter[accessMapCloud[guiConfig.points.axis]];
+
+    pointsColors = pointCloudColorsByDepthComparison(
+      points,
+      guiConfig.points.axis,
+      planeCenterPos
+    );
+  } else {
+    pointsColors = pointCloudColorsByDepth(points, guiConfig.points.axis);
+  }
+
   points.geometry.setAttribute(
     "color",
-    new Float32BufferAttribute(
-      pointCloudColorsByDepth(points, guiConfig.points.axis),
-      3
-    )
+    new Float32BufferAttribute(pointsColors, 3)
   );
 }
 
@@ -306,7 +438,9 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+const planesGroup = new Group();
 function generatePlanesMeshes() {
+  planesGroup.clear();
   for (const plane of planes) {
     const geometry = new BufferGeometry();
 
@@ -326,45 +460,45 @@ function generatePlanesMeshes() {
     const factor = 1.0 / plane.depth;
 
     const aP = [
-      center.x - df.x * factor - cloudCenter.x + n.x * ra,
-      center.y - df.y * factor - cloudCenter.y + n.y * ra,
-      center.z - df.z * factor - cloudCenter.z + n.z * ra,
+      center.x - df.x * factor + n.x * ra,
+      center.y - df.y * factor + n.y * ra,
+      center.z - df.z * factor + n.z * ra,
     ];
     const bP = [
-      center.x - ds.x * factor - cloudCenter.x + n.x * ra,
-      center.y - ds.y * factor - cloudCenter.y + n.y * ra,
-      center.z - ds.z * factor - cloudCenter.z + n.z * ra,
+      center.x - ds.x * factor + n.x * ra,
+      center.y - ds.y * factor + n.y * ra,
+      center.z - ds.z * factor + n.z * ra,
     ];
     const cP = [
-      center.x + df.x * factor - cloudCenter.x + n.x * ra,
-      center.y + df.y * factor - cloudCenter.y + n.y * ra,
-      center.z + df.z * factor - cloudCenter.z + n.z * ra,
+      center.x + df.x * factor + n.x * ra,
+      center.y + df.y * factor + n.y * ra,
+      center.z + df.z * factor + n.z * ra,
     ];
     const dP = [
-      center.x + ds.x * factor - cloudCenter.x + n.x * ra,
-      center.y + ds.y * factor - cloudCenter.y + n.y * ra,
-      center.z + ds.z * factor - cloudCenter.z + n.z * ra,
+      center.x + ds.x * factor + n.x * ra,
+      center.y + ds.y * factor + n.y * ra,
+      center.z + ds.z * factor + n.z * ra,
     ];
 
     const aN = [
-      center.x - df.x * factor - cloudCenter.x - +n.x * ra,
-      center.y - df.y * factor - cloudCenter.y - +n.y * ra,
-      center.z - df.z * factor - cloudCenter.z - +n.z * ra,
+      center.x - df.x * factor - n.x * ra,
+      center.y - df.y * factor - n.y * ra,
+      center.z - df.z * factor - n.z * ra,
     ];
     const bN = [
-      center.x - ds.x * factor - cloudCenter.x - n.x * ra,
-      center.y - ds.y * factor - cloudCenter.y - n.y * ra,
-      center.z - ds.z * factor - cloudCenter.z - n.z * ra,
+      center.x - ds.x * factor - n.x * ra,
+      center.y - ds.y * factor - n.y * ra,
+      center.z - ds.z * factor - n.z * ra,
     ];
     const cN = [
-      center.x + df.x * factor - cloudCenter.x - n.x * ra,
-      center.y + df.y * factor - cloudCenter.y - n.y * ra,
-      center.z + df.z * factor - cloudCenter.z - n.z * ra,
+      center.x + df.x * factor - n.x * ra,
+      center.y + df.y * factor - n.y * ra,
+      center.z + df.z * factor - n.z * ra,
     ];
     const dN = [
-      center.x + ds.x * factor - cloudCenter.x - n.x * ra,
-      center.y + ds.y * factor - cloudCenter.y - n.y * ra,
-      center.z + ds.z * factor - cloudCenter.z - n.z * ra,
+      center.x + ds.x * factor - n.x * ra,
+      center.y + ds.y * factor - n.y * ra,
+      center.z + ds.z * factor - n.z * ra,
     ];
 
     const vertices = new Float32Array([
@@ -400,8 +534,10 @@ function generatePlanesMeshes() {
     const mesh = new Mesh(geometry, material);
 
     planeMeshes.push({ mesh, visible: true });
-    scene.add(mesh);
+    planesGroup.add(mesh);
   }
+
+  scene.add(planesGroup);
 }
 
 function configurePlanesGUI() {
@@ -413,13 +549,9 @@ function configurePlanesGUI() {
 function changePlanesVisibility() {
   console.log("Change visibility to", guiConfig.planes.visible);
   if (guiConfig.planes.visible) {
-    for (const mesh of planeMeshes) {
-      scene.add(mesh.mesh);
-    }
+    scene.add(planesGroup);
   } else {
-    for (const mesh of planeMeshes) {
-      scene.remove(mesh.mesh);
-    }
+    scene.remove(planesGroup);
   }
 }
 
@@ -434,25 +566,23 @@ function animate() {
 }
 
 function checkMouseOver() {
-  raycaster.setFromCamera(mouse, camera);
-
-  // create an array containing all objects in the scene with which the ray intersects
-  const intersects = raycaster.intersectObjects(
-    scene.children.filter((el) => el.type === "Mesh")
-  );
-
-  if (intersects.length > 0) {
-    if (intersected) {
-      if (intersected === intersects[0]) return;
-      intersected.material.color.set(intersected.oldHex);
-    }
-    intersected = intersects[0].object;
-    intersected.oldHex = intersected.material.color.getHex();
-    intersected.material.color.set(0xff0000);
-  } else {
-    if (intersected) {
-      intersected.material.color.set(intersected.oldHex);
-    }
-    intersected = null;
-  }
+  // raycaster.setFromCamera(mouse, camera);
+  // // create an array containing all objects in the scene with which the ray intersects
+  // const intersects = raycaster.intersectObjects(
+  //   scene.children.filter((el) => el.type === "Mesh")
+  // );
+  // if (intersects.length > 0) {
+  //   if (intersected) {
+  //     if (intersected === intersects[0]) return;
+  //     intersected.material.color.set(intersected.oldHex);
+  //   }
+  //   intersected = intersects[0].object;
+  //   intersected.oldHex = intersected.material.color.getHex();
+  //   intersected.material.color.set(0xff0000);
+  // } else {
+  //   if (intersected) {
+  //     intersected.material.color.set(intersected.oldHex);
+  //   }
+  //   intersected = null;
+  // }
 }
